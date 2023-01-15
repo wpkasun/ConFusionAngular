b720001f14302398a0cabf51ff5195435368ce63
Kasun Sameera <wpkasun@gmail.com>
2023-01-15 01:24:12 +0100

    Single Page Applications Part 1

--- src/app/app-routing/routes.ts
+++ src/app/app-routing/routes.ts
@@ -8,6 +8,7 @@ import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
+    { path: 'contactus', component: ContactComponent },
     { path: 'menu', component: MenuComponent },
     { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
 
@@ -45,7 +46,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
