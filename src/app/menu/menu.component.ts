28ee81fa23e551dcbe65ee6163d1075304253202
Kasun Sameera <wpkasun@gmail.com>
2023-05-09 23:56:45 +0200

    Promise Part 1

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -14,6 +14,6 @@ export class MenuComponent implements OnInit {
   constructor(private dishService: DishService) { }
 
   ngOnInit() {
-    this.dishes = this.dishService.getDishes();
+    this.dishService.getDishes().then(dishes => this.dishes = dishes);
   }
 }
